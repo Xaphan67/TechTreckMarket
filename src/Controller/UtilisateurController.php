62414ab15cc9b5d0d136c304d75cac842fdc3112
Xaphan67 <cedric.falda@gmail.com>
2023-12-21 19:55:56 +0100

    Add User change password form

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use App\Form\AdresseType;
 use App\Form\InfosUtilisateurType;
+use Symfony\Component\Form\FormError;
 use App\Form\MotDePasseUtilisateurType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UtilisateurController extends AbstractController
 {
@@ -72,7 +74,7 @@ class UtilisateurController extends AbstractController
     }
 
     #[Route('utilisateur/modifier', name:'modifier_donnees_utilisateur')]
-    public function edit(Request $request, EntityManagerInterface $entityManager)
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
     {
         // Vérifie qu'un utilisateur est connecté
         if ($this->getUser()) {
@@ -91,6 +93,41 @@ class UtilisateurController extends AbstractController
                 // Met à jour l'utilisateur en base de donnée
                 $entityManager->persist($formData);
                 $entityManager->flush();
+            }
+        }
+
+        // Redirige vers le profil de l'utilisateur
+        return $this->redirectToRoute('profil_utilisateur');
+    }
+
+    #[Route('utilisateur/modifierMdp', name:'modifier_mdp_utilisateur')]
+    public function editMdp(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // Vérifie qu'un utilisateur est connecté
+        if ($this->getUser()) {
+            // Récupère l'utilisateur actuellement connecté
+            $utilisateur = $this->getUser();
+
+            // Génère et récupère les informations envoyés via le formulaire
+            $form = $this->createForm(MotDePasseUtilisateurType::class, $utilisateur);
+            $form->handleRequest($request);
+
+            // Vérifie que le formulaire est soumis et est valide
+            if ($form->isSubmitted() && $form->isValid()) {
+                // Récupère les informations du formulaire
+                $ancienMdp = $form->get('oldPassword')->getData();
+                $nouveauMdp = $form->get('password')->getData();
+
+                // Vérifie si le mot de passe saisi correspond au mot de passe actuel de l'utilisateur
+                if ($passwordHasher->isPasswordValid($utilisateur, $ancienMdp)) {
+                    // Hashe le nouveau mot de passe et modifie le mot de passe actuel de l'utilisateur
+                    $nouveauMdpHash = $passwordHasher->hashPassword($utilisateur, $nouveauMdp);
+                    $utilisateur->setPassword($nouveauMdpHash);
+
+                    // Met à jour l'utilisateur en base de donnée
+                    $entityManager->persist($utilisateur);
+                    $entityManager->flush();
+                }
             }
         }
 
--- src/Form/MotDePasseUtilisateurType.php
+++ src/Form/MotDePasseUtilisateurType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -15,9 +16,13 @@ class MotDePasseUtilisateurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('oldPassword', PasswordType::class, [
+                'label' => 'Ancien mot de passe',
+                'mapped' => false
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'invalid_message' => 'Les deux champs Nouveau Mot de passe doivent être identiques.',
+                'invalid_message' => 'Les deux champs pour le nouveau mot de passe doivent être identiques.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
                 'first_options' => [
@@ -29,6 +34,13 @@ class MotDePasseUtilisateurType extends AbstractType
                 'attr' => [
                     'autocomplete' => 'off'
                 ],
+                /*'constraints' => [
+                    new Regex([
+                        'pattern' => '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$/',
+                        'message' => 'Votre mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.'
+                    ])
+                ]*/
+                'mapped' => false
             ])
             ->add('Valider', SubmitType::class, [
                 'attr' => [
