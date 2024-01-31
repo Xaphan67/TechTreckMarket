81156e18b0770c1fb7d9c7a2733d3a34b6927a46
Xaphan67 <cedric.falda@gmail.com>
2024-01-31 13:33:47 +0100

    Fix PC builder product list when in multiple sub-categories

--- src/Controller/ConfigurateurController.php
+++ src/Controller/ConfigurateurController.php
@@ -62,14 +62,17 @@ class ConfigurateurController extends AbstractController
         if (count($categorie->getSousCategories()) > 0) {
             // Si oui, ajoute tout les produit de chaque sous-catégorie
             foreach ($categorie->getSousCategories() as $sousCategorie) {
-                $produits += $produitRepository->findBy([
+                $produitsCategorie = $produitRepository->findBy([
                         'categorie' => $sousCategorie,
                         "archive" => false
                     ]);
-            }
+                    foreach ($produitsCategorie as $produit) {
+                            $produits[] = $produit;
+                    }       
+                }
         } else {
             // Récupère les produits correspondant à la catégorie
-            $produits = $produitRepository->findBy([
+            $produits[] = $produitRepository->findBy([
                     'categorie' => $categorie,
                     "archive" => false
                 ], [
